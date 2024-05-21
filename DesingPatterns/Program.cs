0b805488ea8f6890f60efffa2e3a4cd13f428900
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-05-20 20:18:51 -0600

    add-Proxy

--- DesingPatterns/Program.cs
+++ DesingPatterns/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// La interfaz común para Mr. Link (RealSubject) y Mr. Proxy
+public interface IBoxSafe
+{
+    void OpenSafe();
+}
+
+// El objeto real que el proxy representa
+public class MrLink : IBoxSafe
+{
+    public void OpenSafe()
+    {
+        Console.WriteLine("Mr. Link: Opening the safe.");
+    }
+}
+
+// El proxy que controla el acceso al Mr. Link
+public class MrProxy : IBoxSafe
+{
+    private MrLink _mrLink;
+
+    private bool VerifyAccess()
+    {
+        // Verifica el acceso antes de proceder
+        Console.WriteLine("Mr. Proxy: Verifying access to the safe.");
+        return true;
+    }
+
+    private void LogAccess()
+    {
+        // Registro del acceso
+        Console.WriteLine("Mr. Proxy: Logging the access time and reason.");
+    }
+
+    public void OpenSafe()
+    {
+        if (VerifyAccess())
+        {
+            if (_mrLink == null)
+            {
+                _mrLink = new MrLink();
+            }
+            _mrLink.OpenSafe();
+            LogAccess();
+        }
+    }
+}
+
+// Uso del Proxy
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Client: Trying to access the safe with Mr. Link directly:");
+        MrLink mrLink = new MrLink();
+        mrLink.OpenSafe();
+
+        Console.WriteLine("\nClient: Trying to access the safe with Mr. Proxy:");
+        MrProxy mrProxy = new MrProxy();
+        mrProxy.OpenSafe();
+    }
+}
---
+++ DesingPatterns/StructuralPatterns/Proxy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPatterns.StructuralPatterns
+{
+    public interface IBoxSafe
+    {
+        void OpenSafe();
+    }
+
+    // El objeto real que el proxy representa
+    public class MrLink : IBoxSafe
+    {
+        public void OpenSafe()
+        {
+            Console.WriteLine("Mr. Link: Opening the safe.");
+        }
+    }
+
+    // El proxy que controla el acceso al Mr. Link
+    public class MrProxy : IBoxSafe
+    {
+        private MrLink _mrLink;
+
+        private bool VerifyAccess()
+        {
+            // Verifica el acceso antes de proceder
+            Console.WriteLine("Mr. Proxy: Verifying access to the safe.");
+            return true;
+        }
+
+        private void LogAccess()
+        {
+            // Registro del acceso
+            Console.WriteLine("Mr. Proxy: Logging the access time and reason.");
+        }
+
+        public void OpenSafe()
+        {
+            if (VerifyAccess())
+            {
+                if (_mrLink == null)
+                {
+                    _mrLink = new MrLink();
+                }
+                _mrLink.OpenSafe();
+                LogAccess();
+            }
+        }
+    }
+}
+
+
+
+// La interfaz común para Mr. Link (RealSubject) y Mr. Proxy
+
+
+// Uso del Proxy
+/*class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Client: Trying to access the safe with Mr. Link directly:");
+        MrLink mrLink = new MrLink();
+        mrLink.OpenSafe();
+
+        Console.WriteLine("\nClient: Trying to access the safe with Mr. Proxy:");
+        MrProxy mrProxy = new MrProxy();
+        mrProxy.OpenSafe();
+    }
+}*/
